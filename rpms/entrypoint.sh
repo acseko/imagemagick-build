bde727fb0262a5ae9c268ed4267d87c79d6d0c90
András Csekő <acseko@users.noreply.github.com>
2024-09-13 22:31:51 +0200

    Update entrypoint.sh

--- rpms/entrypoint.sh
+++ rpms/entrypoint.sh
@@ -40,8 +40,8 @@ sed -i '/BuildRequires.*LibRaw/d; /--with-raw/d' ImageMagick.spec.in
 sed -i '/BuildRequires.*urw-base35-fonts-devel/d; /--with-urw-base35-font-dir=.*$/d' ImageMagick.spec.in
 
 #libheif is not available on el8.
-sed -i 's/%if %{with libheif}/%if %{with libheif} && 0%{?rhel} > 8/g' ImageMagick.spec.in
-grep -A5 -B 5 libheif ImageMagick.spec.in
+sed -i 's/%if %{with libheif}$/%if %{with libheif} \&\& 0%{?rhel} > 8/g' ImageMagick.spec.in
+grep -A 5 -B 5 libheif ImageMagick.spec.in
 
 AFTER_CHECKOUT_HOOK_SCRIPT="../after-checkout-${BASE_IMAGE//:/}-$IMAGEMAGICK_VERSION.sh"
 if [ -x "$AFTER_CHECKOUT_HOOK_SCRIPT" ]; then
